15d8e8a12c1fb67950c88f8613998d42fafc9ad9
Kou <zhentao.kou@intel.com>
2025-05-10 10:20:04 +0800

    elf

--- configs/learning_gem5/part1/two_level.py
+++ configs/learning_gem5/part1/two_level.py
@@ -58,7 +58,8 @@ thispath = os.path.dirname(os.path.realpath(__file__))
 default_binary = os.path.join(
     thispath,
     "../../../",
-    "tests/test-progs/hello/bin/x86/linux/hello",
+    "tests/test-progs/hello/bin/riscv/linux/coremark.riscv",
+    #   "tests/test-progs/hello/bin/x86/linux/hello",
 )
 
 # Binary to execute
@@ -80,7 +81,8 @@ system.mem_mode = "timing"  # Use timing accesses
 system.mem_ranges = [AddrRange("512MiB")]  # Create an address range
 
 # Create a simple CPU
-system.cpu = X86TimingSimpleCPU()
+#system.cpu = X86TimingSimpleCPU()
+system.cpu = RiscvMinorCPU()
 
 # Create an L1 instruction and data cache
 system.cpu.icache = L1ICache(args)
@@ -109,9 +111,9 @@ system.l2cache.connectMemSideBus(system.membus)
 
 # create the interrupt controller for the CPU
 system.cpu.createInterruptController()
-system.cpu.interrupts[0].pio = system.membus.mem_side_ports
-system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
-system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+# system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+# system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+# system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
 
 # Connect the system up to the membus
 system.system_port = system.membus.cpu_side_ports
